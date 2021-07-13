4725659cc28dedc512f33696736dc97da3cf7b86
hynzlee <hji0104@kaist.ac.kr>
2021-07-14 03:15:26 +0900

    tab2 dialog fix

--- app/src/main/java/com/sample/fortest/Fragment2.java
+++ app/src/main/java/com/sample/fortest/Fragment2.java
@@ -24,11 +24,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import com.sample.fortest.DateUtil;
+
 public class Fragment2 extends Fragment {
     ImageButton button;
     Dialog dialog;
     View view;
     String gusetName= "";
+    ArrayList<String> guestList;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -46,7 +49,7 @@ public class Fragment2 extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showDialog(); // 아래 showDialog01() 함수 호출
+                showDialog(); // 아래 showDialog() 함수 호출
             }
         });
 
@@ -62,25 +65,39 @@ public class Fragment2 extends Fragment {
             st += arrayList.get(i) + "\n";
         tx.setText(st);
     }
+    public void resetText(){
+        EditText editText1 = dialog.findViewById(R.id.edit_roomName);
+        EditText editText2 = dialog.findViewById(R.id.edit_fine);
+        EditText editText3 = dialog.findViewById(R.id.edit_textDate);
+        editText1.setText("");
+        editText2.setText("");
+        editText3.setText("");
+    }
 
     public void showDialog(){
         dialog.show(); // 다이얼로그 띄우기
-
-
-
 
         /* 이 함수 안에 원하는 디자인과 기능을 구현하면 된다. */
         // *주의할 점: findViewById()를 쓸 때는 -> 앞에 반드시 다이얼로그 이름을 붙여야 한다.
 
         ArrayList<HashMap<String, String>> array = ((MainActivity)getActivity()).idListArray();
-        ArrayList<String> guestList = new ArrayList<>();
+        HashMap<String, String> roomhash = new HashMap<>();
+        guestList = new ArrayList<>();
         gusetName = "";
 
 
         //mail 버튼
         EditText editText = dialog.findViewById(R.id.guestMail);
+
         Button gBtn = dialog.findViewById(R.id.guestButton);
+        Button aBtn = dialog.findViewById(R.id.addButton);
+
         TextView tx = dialog.findViewById(R.id.guestList);
+
+        EditText editText1 = dialog.findViewById(R.id.edit_roomName);
+        EditText editText2 = dialog.findViewById(R.id.edit_fine);
+        EditText editText3 = dialog.findViewById(R.id.edit_textDate);
+
         gBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -92,14 +109,32 @@ public class Fragment2 extends Fragment {
                         if (array.get(i).get("id").equals(input)) {
                             guestList.add(input);
                             SetText(tx, guestList);
-                            Toast.makeText(getContext(), input, Toast.LENGTH_SHORT).show();
+                            editText.setText("");
+                            //Toast.makeText(getContext(), input, Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(getContext(), "No User", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
+            }
+        });
 
-
+        aBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int gcount = guestList.size();
+                String guest = "guest";
+                for(int i = 0; i<gcount; i++){
+                    roomhash.put(guest+Integer.toString(i+1),guestList.get(i));
+                }
+                roomhash.put("RoomName",editText1.getText().toString());
+                String startday = editText2.getText().toString();
+                roomhash.put("startDay",startday);
+                roomhash.put("fine",editText3.getText().toString());
+                array.add(roomhash);
+                resetText();
+                tx.setText("");
+                dialog.dismiss();
             }
         });
     }
