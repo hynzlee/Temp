fb953d44ff1e621692d8b62cb162694d72afad4d
hynzlee <hji0104@kaist.ac.kr>
2021-07-14 10:26:40 +0900

    tab3

--- app/src/main/java/com/sample/fortest/Fragment3.java
+++ app/src/main/java/com/sample/fortest/Fragment3.java
@@ -8,29 +8,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 
-import com.sample.fortest.DateUtil;
 
 public class Fragment3 extends Fragment {
-    ListView listView;
+    TextView username1, username2, username3, username4;
+    TextView fine1, fine2, fine3, fine4;
+    ImageView iv_1, iv_2, iv_3, iv_4;
     View view;
     String guest0,guest1,guest2,guest3;
     ContentAdapter contentAdapter;
     Button button;
     String clickDay;
     HashMap<String, String> rommHashMap;
+
 
     private Intent intent;
     @Override
@@ -55,9 +51,24 @@ public class Fragment3 extends Fragment {
                 days += (month < 10)? "0"+Integer.toString(month+1) : Integer.toString(month+1);
                 days += (dayOfMonth < 10)? "0"+Integer.toString(dayOfMonth) : Integer.toString(dayOfMonth);
                 Log.e("day",days);
+
+                fine1 = (TextView) view.findViewById(R.id.Fine1);
+                fine2 = (TextView) view.findViewById(R.id.Fine2);
+                fine3 = (TextView) view.findViewById(R.id.Fine3);
+                fine4 = (TextView) view.findViewById(R.id.Fine4);
+
+                username1 = (TextView) view.findViewById(R.id.User1);
+                username2 = (TextView) view.findViewById(R.id.User2);
+                username3 = (TextView) view.findViewById(R.id.User3);
+                username4 = (TextView) view.findViewById(R.id.User4);
+
+                iv_1 = (ImageView) view.findViewById(R.id.userImage1);
+                iv_2 = (ImageView) view.findViewById(R.id.userImage2);
+                iv_3 = (ImageView) view.findViewById(R.id.userImage3);
+                iv_4 = (ImageView) view.findViewById(R.id.userImage4);
+
             }
         });
-        listView = (ListView) view.findViewById(R.id.list3);
 
 
         return view;
@@ -65,5 +76,7 @@ public class Fragment3 extends Fragment {
     public void setHash(HashMap<String, String> map){
         this.rommHashMap = map;
     }
+
+
 
 }
