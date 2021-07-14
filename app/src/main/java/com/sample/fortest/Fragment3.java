7e2029822bd7acfb355b95c3368acd70aa695ebd
jsuk10 <jsuk970407@gmail.com>
2021-07-14 10:26:20 +0900

    feat : Passing data to tab 3

--- app/src/main/java/com/sample/fortest/Fragment3.java
+++ app/src/main/java/com/sample/fortest/Fragment3.java
@@ -26,44 +26,73 @@ import com.sample.fortest.DateUtil;
 public class Fragment3 extends Fragment {
     ListView listView;
     View view;
-    String guest0,guest1,guest2,guest3;
+    String guest0, guest1, guest2, guest3;
     ContentAdapter contentAdapter;
     Button button;
     String clickDay;
     HashMap<String, String> rommHashMap;
+    ArrayList<TodoData> todoList;
+    Integer guestCount = 0;
+    //데이, 이미지값 들어
+    HashMap<String, String> hostHash;
+    HashMap<String, String> guest1Hash;
+    HashMap<String, String> guest2Hash;
+    HashMap<String, String> guest3Hash;
 
     private Intent intent;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_3, container, false);
 
-        //현지 임의 데이
-        //ArrayList<HashMap<String, String>> roomArray = roomArray();
-        //ArrayList<HashMap<String, String>> todoArray = roomArray();
-        //ArrayList<String> emailArray = new ArrayList<>();
-        //HashMap<String, String> hash = roomArray.get(0);
 
-        CalendarView  calendar = (CalendarView) view.findViewById(R.id.calendarView);
+        CalendarView calendar = (CalendarView) view.findViewById(R.id.calendarView);
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String days = "";
                 year /= 100;
-                days += (year < 10)? "0"+ Integer.toString(year) : Integer.toString(year);
-                days += (month < 10)? "0"+Integer.toString(month+1) : Integer.toString(month+1);
-                days += (dayOfMonth < 10)? "0"+Integer.toString(dayOfMonth) : Integer.toString(dayOfMonth);
-                Log.e("day",days);
+                days += (year < 10) ? "0" + Integer.toString(year) : Integer.toString(year);
+                days += (month < 10) ? "0" + Integer.toString(month + 1) : Integer.toString(month + 1);
+                days += (dayOfMonth < 10) ? "0" + Integer.toString(dayOfMonth) : Integer.toString(dayOfMonth);
+                Log.e("day", days);
+                //투두리스트에서 해당 이미지 뽑아오지
+
             }
         });
         listView = (ListView) view.findViewById(R.id.list3);
+        //룸데이타에서 fines 보여주기
 
 
         return view;
     }
-    public void setHash(HashMap<String, String> map){
+
+    public void setHash(HashMap<String, String> map) {
         this.rommHashMap = map;
     }
 
+    public void setData(ArrayList<HashMap<String, String>> todoList, HashMap<String,String> roomdata) {
+        hostHash = new HashMap<>();
+        guest1Hash = new HashMap<>();
+        guest2Hash = new HashMap<>();
+        guest3Hash = new HashMap<>();
+        int maxindex = 0;
+        for (int i = 0; i < todoList.size(); i++) {
+            if (todoList.get(i).get("id") == roomdata.get("id")) {
+                hostHash.put(todoList.get(i).get("date"), todoList.get(i).get("photo"));
+            } else if (todoList.get(i).get("id") == roomdata.get("guest1") ){
+                maxindex = maxindex > 1 ? maxindex : 1;
+                guest1Hash.put(todoList.get(i).get("date"), todoList.get(i).get("photo"));
+            } else if (todoList.get(i).get("id") == roomdata.get("guest2")) {
+                maxindex = maxindex > 2 ? maxindex : 2;
+                guest2Hash.put(todoList.get(i).get("date"), todoList.get(i).get("photo"));
+            } else if (todoList.get(i).get("id") == roomdata.get("guest3")) {
+                maxindex = 3;
+                guest3Hash.put(todoList.get(i).get("date"), todoList.get(i).get("photo"));
+            }
+            this.rommHashMap = roomdata;
+        }
+    }
 }
